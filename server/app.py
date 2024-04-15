6b729ef784a2e4081e84b759c0c2e6fd70b5962e
Koby Fink <finkkoby@icloud.com>
2024-04-15 15:54:14 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User
 
@@ -28,16 +27,33 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+
+    def get(self):
+        _id = session.get('user_id')
+        if _id:
+            user = User.query.filter(User.id == _id).first()
+            return make_response(user.to_dict(), 200)
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
